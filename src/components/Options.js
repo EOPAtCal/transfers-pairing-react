3604babd971380bcc251e1155bc4f244d178771a
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-09-18 15:57:10 -0700

    checkpoint

--- src/components/Options.js
+++ src/components/Options.js
@@ -32,232 +32,269 @@ class Options extends PureComponent {
       menteeMajor
     } = this.state;
     return (
-      <div>
-        <h2 className="uk-text-capitalize uk-margin-small-bottom">options</h2>
-        <h4 className="uk-margin-remove-top">
-          Be careful, what you modify here will affect the matching results.
-        </h4>
-        <hr className="uk-divider-icon" />
-        <form
-          className="uk-text-left uk-form-horizontal"
-          onSubmit={this.handleSubmit}
-        >
-          <div className="uk-grid-small" uk-grid="">
-            <div className="uk-width-1-2@s">
-              <fieldset className="uk-fieldset">
-                <legend className="uk-legend uk-text-capitalize">
-                  mentor spreadsheet
-                </legend>
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="form-stacked-text">
-                    spreadsheet id
-                  </label>
-                  <div className="uk-form-controls">
-                    <input
-                      name="mentorSpreadsheetId"
-                      className="uk-input uk-form-width-small"
-                      type="text"
-                      required
-                      value={mentorSpreadsheetId}
-                      onChange={this.handleChange}
-                    />
+      <div className="uk-section uk-section-default uk-section-small">
+        <div className="uk-container uk-container-expand">
+          <h2 className="uk-text-lead uk-text-uppercase uk-margin-small-bottom">
+            options
+          </h2>
+          <h4
+            className="uk-margin-remove-top uk-text-muted"
+            style={{ fontWeight: 300 }}
+          >
+            Be careful, what you modify here will affect the matching results.
+          </h4>
+          <hr />
+          <form
+            className="uk-text-left uk-form-horizontal"
+            onSubmit={this.handleSubmit}
+          >
+            <div className="uk-grid-small" uk-grid="">
+              <div className="uk-width-1-2@s">
+                <fieldset className="uk-fieldset">
+                  <legend className="uk-legend uk-text-capitalize">
+                    mentor spreadsheet
+                  </legend>
+                  <div className="uk-margin">
+                    <label
+                      className="uk-form-label"
+                      htmlFor="form-stacked-text"
+                    >
+                      spreadsheet id
+                    </label>
+                    <div className="uk-form-controls">
+                      <input
+                        name="mentorSpreadsheetId"
+                        className="uk-input uk-form-width-small"
+                        type="text"
+                        required
+                        value={mentorSpreadsheetId}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="form-stacked-text">
-                    spreadsheet range
-                  </label>
-                  <div className="uk-form-controls">
-                    <input
-                      name="mentorRange"
-                      className="uk-input uk-form-width-small"
-                      type="text"
-                      required
-                      value={mentorRange}
-                      onChange={this.handleChange}
-                    />
+                  <div className="uk-margin">
+                    <label
+                      className="uk-form-label"
+                      htmlFor="form-stacked-text"
+                    >
+                      spreadsheet range
+                    </label>
+                    <div className="uk-form-controls">
+                      <input
+                        name="mentorRange"
+                        className="uk-input uk-form-width-small"
+                        type="text"
+                        required
+                        value={mentorRange}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-              </fieldset>
-              <fieldset className="uk-fieldset">
-                <legend className="uk-legend uk-text-capitalize">
-                  mentor data position
-                </legend>
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="form-stacked-text">
-                    email
-                  </label>
-                  <div className="uk-form-controls">
-                    <input
-                      name="mentorEmail"
-                      className="uk-input uk-form-width-small"
-                      type="number"
-                      required
-                      value={mentorEmail}
-                      onChange={this.handleChange}
-                    />
+                </fieldset>
+                <fieldset className="uk-fieldset">
+                  <legend className="uk-legend uk-text-capitalize">
+                    mentor data position
+                  </legend>
+                  <div className="uk-margin">
+                    <label
+                      className="uk-form-label"
+                      htmlFor="form-stacked-text"
+                    >
+                      email
+                    </label>
+                    <div className="uk-form-controls">
+                      <input
+                        name="mentorEmail"
+                        className="uk-input uk-form-width-small"
+                        type="number"
+                        required
+                        value={mentorEmail}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="form-stacked-text">
-                    college
-                  </label>
-                  <div className="uk-form-controls">
-                    <input
-                      name="mentorCollege"
-                      className="uk-input uk-form-width-small"
-                      type="number"
-                      required
-                      value={mentorCollege}
-                      onChange={this.handleChange}
-                    />
+                  <div className="uk-margin">
+                    <label
+                      className="uk-form-label"
+                      htmlFor="form-stacked-text"
+                    >
+                      college
+                    </label>
+                    <div className="uk-form-controls">
+                      <input
+                        name="mentorCollege"
+                        className="uk-input uk-form-width-small"
+                        type="number"
+                        required
+                        value={mentorCollege}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="form-stacked-text">
-                    major
-                  </label>
-                  <div className="uk-form-controls">
-                    <input
-                      name="mentorMajor"
-                      className="uk-input uk-form-width-small"
-                      type="number"
-                      required
-                      value={mentorMajor}
-                      onChange={this.handleChange}
-                    />
+                  <div className="uk-margin">
+                    <label
+                      className="uk-form-label"
+                      htmlFor="form-stacked-text"
+                    >
+                      major
+                    </label>
+                    <div className="uk-form-controls">
+                      <input
+                        name="mentorMajor"
+                        className="uk-input uk-form-width-small"
+                        type="number"
+                        required
+                        value={mentorMajor}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-              </fieldset>
-            </div>
-            <div className="uk-width-1-2@s">
-              <fieldset className="uk-fieldset">
-                <legend className="uk-legend uk-text-capitalize">
-                  mentee spreadsheet
-                </legend>
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="form-stacked-text">
-                    spreadsheet id
-                  </label>
-                  <div className="uk-form-controls">
-                    <input
-                      name="menteeSpreadsheetId"
-                      className="uk-input uk-form-width-small"
-                      type="text"
-                      required
-                      value={menteeSpreadsheetId}
-                      onChange={this.handleChange}
-                    />
+                </fieldset>
+              </div>
+              <div className="uk-width-1-2@s">
+                <fieldset className="uk-fieldset">
+                  <legend className="uk-legend uk-text-capitalize">
+                    mentee spreadsheet
+                  </legend>
+                  <div className="uk-margin">
+                    <label
+                      className="uk-form-label"
+                      htmlFor="form-stacked-text"
+                    >
+                      spreadsheet id
+                    </label>
+                    <div className="uk-form-controls">
+                      <input
+                        name="menteeSpreadsheetId"
+                        className="uk-input uk-form-width-small"
+                        type="text"
+                        required
+                        value={menteeSpreadsheetId}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="form-stacked-text">
-                    spreadsheet range
-                  </label>
-                  <div className="uk-form-controls">
-                    <input
-                      name="menteeRange"
-                      className="uk-input uk-form-width-small"
-                      type="text"
-                      required
-                      value={menteeRange}
-                      onChange={this.handleChange}
-                    />
+                  <div className="uk-margin">
+                    <label
+                      className="uk-form-label"
+                      htmlFor="form-stacked-text"
+                    >
+                      spreadsheet range
+                    </label>
+                    <div className="uk-form-controls">
+                      <input
+                        name="menteeRange"
+                        className="uk-input uk-form-width-small"
+                        type="text"
+                        required
+                        value={menteeRange}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-              </fieldset>
-              <fieldset className="uk-fieldset">
-                <legend className="uk-legend uk-text-capitalize">
-                  mentee data position
-                </legend>
+                </fieldset>
+                <fieldset className="uk-fieldset">
+                  <legend className="uk-legend uk-text-capitalize">
+                    mentee data position
+                  </legend>
 
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="form-stacked-text">
-                    email
-                  </label>
-                  <div className="uk-form-controls">
-                    <input
-                      name="menteeEmail"
-                      className="uk-input uk-form-width-small"
-                      type="number"
-                      required
-                      value={menteeEmail}
-                      onChange={this.handleChange}
-                    />
+                  <div className="uk-margin">
+                    <label
+                      className="uk-form-label"
+                      htmlFor="form-stacked-text"
+                    >
+                      email
+                    </label>
+                    <div className="uk-form-controls">
+                      <input
+                        name="menteeEmail"
+                        className="uk-input uk-form-width-small"
+                        type="number"
+                        required
+                        value={menteeEmail}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="form-stacked-text">
-                    college
-                  </label>
-                  <div className="uk-form-controls">
-                    <input
-                      name="menteeCollege"
-                      className="uk-input uk-form-width-small"
-                      type="number"
-                      required
-                      value={menteeCollege}
-                      onChange={this.handleChange}
-                    />
+                  <div className="uk-margin">
+                    <label
+                      className="uk-form-label"
+                      htmlFor="form-stacked-text"
+                    >
+                      college
+                    </label>
+                    <div className="uk-form-controls">
+                      <input
+                        name="menteeCollege"
+                        className="uk-input uk-form-width-small"
+                        type="number"
+                        required
+                        value={menteeCollege}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="form-stacked-text">
-                    major
-                  </label>
-                  <div className="uk-form-controls">
-                    <input
-                      name="menteeMajor"
-                      className="uk-input uk-form-width-small"
-                      type="number"
-                      required
-                      value={menteeMajor}
-                      onChange={this.handleChange}
-                    />
+                  <div className="uk-margin">
+                    <label
+                      className="uk-form-label"
+                      htmlFor="form-stacked-text"
+                    >
+                      major
+                    </label>
+                    <div className="uk-form-controls">
+                      <input
+                        name="menteeMajor"
+                        className="uk-input uk-form-width-small"
+                        type="number"
+                        required
+                        value={menteeMajor}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-              </fieldset>
+                </fieldset>
+              </div>
             </div>
-          </div>
-          <fieldset className="uk-fieldset">
-            <legend className="uk-legend uk-text-capitalize">
-              match criteria
-            </legend>
-            <div className="uk-margin">
-              <label>
+            <fieldset className="uk-fieldset">
+              <legend className="uk-legend uk-text-capitalize">
+                match criteria
+              </legend>
+              <div className="uk-margin">
+                <label>
+                  <input
+                    name="matchByMajors"
+                    className="uk-checkbox uk-margin-small-right"
+                    type="checkbox"
+                    checked={matchByMajors}
+                    onChange={this.handleChange}
+                  />
+                  match by majors
+                </label>
+              </div>
+              <div className="uk-margin">
+                <label>
+                  <input
+                    name="matchByColleges"
+                    className="uk-checkbox uk-margin-small-right"
+                    type="checkbox"
+                    checked={matchByColleges}
+                    onChange={this.handleChange}
+                  />
+                  match by colleges
+                </label>
+              </div>
+            </fieldset>
+            <div className="uk-grid-small" uk-grid="">
+              <div className="uk-width-1-2@s">
                 <input
-                  name="matchByMajors"
-                  className="uk-checkbox uk-margin-small-right"
-                  type="checkbox"
-                  checked={matchByMajors}
-                  onChange={this.handleChange}
+                  className="uk-button uk-button-danger"
+                  type="submit"
+                  value="Save & Match"
                 />
-                match by majors
-              </label>
+              </div>
             </div>
-            <div className="uk-margin">
-              <label>
-                <input
-                  name="matchByColleges"
-                  className="uk-checkbox uk-margin-small-right"
-                  type="checkbox"
-                  checked={matchByColleges}
-                  onChange={this.handleChange}
-                />
-                match by colleges
-              </label>
-            </div>
-          </fieldset>
-          <div className="uk-grid-small" uk-grid="">
-            <div className="uk-width-1-2@s">
-              <input
-                className="uk-button uk-button-danger"
-                type="submit"
-                value="Save & Match"
-              />
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     );
   }
