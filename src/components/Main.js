38003cbd900a0ba83d219cdd38195080f515ef59
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-09-20 14:04:52 -0700

    checkpoint

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,6 @@ const getMentorLimit = limit => (limit.charAt(0) === '2' ? 8 : 4);
 
 function InfoCard({ name, email, college, major, limit }) {
   return (
-    <div className="uk-inline">
     <div uk-dropdown="pos: bottom-left; delay-hide: 0; animation: uk-animation-slide-bottom-small; ">
       <div className="uk-card uk-card-body uk-card-small">
         <h3 className="uk-card-title">{name}</h3>
@@ -14,80 +13,82 @@ function InfoCard({ name, email, college, major, limit }) {
         {limit && <div className="uk-label">{getMentorLimit(limit)}</div>}
       </div>
     </div>
-    </div>
   );
 }
 
 const Main = ({ matches, handleMatch, unmatchedMentors, unmatchedMentees }) => (
   <div>
-    <div className="uk-flex-middle uk-flex-between" uk-grid="">
-      <div>
-        <h2 className="uk-text-lead uk-text-uppercase uk-margin-remove">
-          results
-        </h2>
+    <div>
+      <div className="uk-flex-middle uk-flex-between" uk-grid="">
+        <div>
+          <h2 className="uk-text-lead uk-text-uppercase uk-margin-remove">
+            results
+          </h2>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="uk-button uk-button-danger"
+            onClick={handleMatch}
+          >
+            <span uk-icon="cog" className="uk-margin-small-right" /> match
+          </button>
+        </div>
       </div>
-      <div>
-        <button
-          type="submit"
-          className="uk-button uk-button-danger"
-          onClick={handleMatch}
-        >
-          <span uk-icon="cog" className="uk-margin-small-right" /> match
-        </button>
-      </div>
-    </div>
-    <hr />
-    <div
-      className="uk-child-width-1-3@s uk-grid-small uk-grid-match"
-      uk-grid=""
-    >
-      <div>
-        <h2 className="uk-text-capitalize">mentor</h2>
-      </div>
-      <div>
-        <h2 className="uk-text-capitalize">mentees</h2>
-      </div>
-      <div>
-        <h2 className="uk-text-capitalize">reason</h2>
-      </div>
-    </div>
-    {matches.map(({ mentor, mentees, reasons }) => (
+      <hr />
       <div
-        key={mentor.id}
-        className="uk-child-width-1-3@s uk-grid-small uk-grid-match uk-card uk-card-default uk-card-small uk-card-body uk-card-hover"
+        className="uk-child-width-1-3@s uk-grid-small uk-grid-match"
         uk-grid=""
       >
-        <ul className="uk-list">
-            <li>{mentor.id}</li>
-            <InfoCard
-              email={mentor.email}
-              college={mentor.college}
-              major={mentor.major}
-              limit={mentor.limit}
-            />
-          </div>
-        </ul>
-        <ul className="uk-list uk-list-divider">
-          {mentees.map((mentee, idx) => [
-            <li key={idx}>
-              {mentee.id}
-              <button uk-icon="copy" />
-            </li>,
-            <InfoCard
-              key={mentee.email}
-              email={mentee.email}
-              college={mentee.college}
-              major={mentee.major}
-            />
-          ])}
-        </ul>
-        <ul className="uk-list uk-list-divider">
-          {reasons.map((reason, idx) => (
-            <li key={idx}>{reason}</li>
-          ))}
-        </ul>
+        <div>
+          <h2 className="uk-text-capitalize">mentor</h2>
+        </div>
+        <div>
+          <h2 className="uk-text-capitalize">mentees</h2>
+        </div>
+        <div>
+          <h2 className="uk-text-capitalize">reason</h2>
+        </div>
       </div>
-    ))}
+      {matches.map(({ mentor, mentees, reasons }) => (
+        <div
+          key={mentor.id}
+          className="uk-child-width-1-3@s uk-grid-small uk-card uk-card-default uk-card-small uk-card-body uk-card-hover"
+          uk-grid=""
+        >
+          <ul className="uk-list">
+            <div className="uk-inline">
+              <li>{mentor.id}</li>
+              <InfoCard
+                email={mentor.email}
+                college={mentor.college}
+                major={mentor.major}
+                limit={mentor.limit}
+              />
+            </div>
+          </ul>
+          <ul className="uk-list uk-list-divider">
+            {mentees.map((mentee, idx) => [
+              <li key={idx}>
+                {mentee.id}
+                <button uk-icon="copy" />
+              </li>,
+              <InfoCard
+                key={mentee.email}
+                email={mentee.email}
+                college={mentee.college}
+                major={mentee.major}
+              />
+            ])}
+          </ul>
+          <ul className="uk-list uk-list-divider">
+            {reasons.map((reason, idx) => (
+              <li key={idx}>{reason}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
 
     <hr className="uk-divider-icon" />
     {(unmatchedMentees.length > 0 || unmatchedMentors.length > 0) && (
