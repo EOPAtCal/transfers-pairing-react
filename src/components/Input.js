6350e1ae25c770cf67ab00fdb1b93a5668baf201
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-09-24 00:26:41 -0700

    checkpoint

--- src/components/Input.js
+++ src/components/Input.js
@@ -28,20 +28,20 @@ const InputTextUserOptions = ({
   handleRemove,
   isMentor
 }) => (
-  <div className="uk-grid" uk-grid="">
-    <div className="uk-form-label">
-      <input
-        className={
-          'uk-input uk-form-blank uk-form-width-small' +
-          (!label ? ' input-error' : '')
-        }
-        type="text"
-        value={label}
-        onChange={handleChange(2)}
-        required
-      />
-    </div>
-    <div className="uk-flex uk-flex-middle uk-flex-between">
+  <div className="uk-grid uk-flex-middle" uk-grid="">
+    <div className="uk-margin">
+      <div className="uk-form-label">
+        <input
+          className={
+            'uk-input uk-form-blank uk-form-width-small' +
+            (!label ? ' input-error' : '')
+          }
+          type="text"
+          value={label}
+          onChange={handleChange(2)}
+          required
+        />
+      </div>
       <div className="uk-form-controls">
         <input
           className={
@@ -53,14 +53,14 @@ const InputTextUserOptions = ({
           onChange={handleChange(isMentor ? 0 : 1)}
         />
       </div>
-      <div>
-        <button
-          type="button"
-          className="uk-icon-button"
-          uk-icon="minus"
-          onClick={handleRemove}
-        />
-      </div>
+    </div>
+    <div>
+      <button
+        type="button"
+        className="uk-icon-button"
+        uk-icon="minus"
+        onClick={handleRemove}
+      />
     </div>
   </div>
 );
